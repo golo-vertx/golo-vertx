429366386135a23654051e2707b5c834814d80bf
samutamm <samutamm@gmail.com>
2015-10-06 13:40:57 +0200

    Initial structure. Some experimental with Code Generation.

---
+++ src/main/java/io/vertx/golo/package-info.java
@@ -0,0 +1,9 @@
+
+/*
+* Defines a module for @VertxGen-annotated interfaces. Name
+* should follow the artifact-id (defined in pom.xml) of the project.
+* */
+@GenModule(name = "vertx-lang-golo")
+package io.vertx.golo;
+
+import io.vertx.codegen.annotations.GenModule;
